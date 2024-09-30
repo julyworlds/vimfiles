9df0a621d41d505e948bbd6c625c512d71dfa25f
juldiadia <julian.diaz@upinion.com>
2024-09-30 17:39:16 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ ln -s ~/.vim/vimrc ~/.vimrc
 rm -f ~/.tern-project
 ln -s ~/.vim/tern-project ~/.tern-project
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim/
+ln -s ~/.vim/nvim_init.vim ~/.config/nvim/init.vim
 nvim +PlugInstall +qall
